c702bff7f0797932bb5a964cb6168487916995ea
ryo <akstkr@gmail.com>
2023-07-22 04:37:44 +0900

    ドラムロールが鳴るようにする

--- app/src/main/java/com/example/omoshiro_app/view/fragment/ResultFragment.kt
+++ app/src/main/java/com/example/omoshiro_app/view/fragment/ResultFragment.kt
@@ -1,19 +1,29 @@
 package com.example.omoshiro_app.view.fragment
 
+import android.media.AudioAttributes
+import android.media.SoundPool
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import com.example.omoshiro_app.R
 import com.example.omoshiro_app.databinding.FragmentResultBinding
 import com.example.omoshiro_app.viewmodel.HomeViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ResultFragment : Fragment() {
 
     companion object {
         fun newInstance() = ResultFragment()
     }
+
+    private lateinit var soundPool: SoundPool
+    private var sound = 0
 
     private lateinit var binding: FragmentResultBinding
     private lateinit var viewModel: HomeViewModel
@@ -47,7 +57,34 @@ class ResultFragment : Fragment() {
             parentFragmentManager.popBackStack()
         }
 
+        playSE()
+
+        binding.blackView.let {
+            it.visibility = View.VISIBLE
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(2400)
+                it.visibility = View.INVISIBLE
+            }
+        }
         setImage()
+    }
+
+    private fun playSE(){
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_GAME)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+            .build()
+
+        soundPool = SoundPool.Builder()
+            .setAudioAttributes(audioAttributes)
+            .setMaxStreams(1)
+            .build()
+
+        sound = soundPool.load(requireActivity(), R.raw.drum_roll, 1)
+        soundPool.setOnLoadCompleteListener{ soundPool, sampleId, status ->
+            soundPool.play(sound, 1.0f, 1.0f, 0, 0, 1.0f)
+        }
+
     }
 
     private fun setImage() {
