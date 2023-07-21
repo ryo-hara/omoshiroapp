8be4b2ebef479b35728e4f6a1ec5ace6efd871a0
ryo <akstkr@gmail.com>
2023-07-22 00:58:28 +0900

    Splash画面にちょっとした演出を追加する

--- app/src/main/java/com/example/omoshiro_app/view/SplashActivity.kt
+++ app/src/main/java/com/example/omoshiro_app/view/SplashActivity.kt
@@ -2,7 +2,9 @@ package com.example.omoshiro_app.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.omoshiro_app.R
 import com.example.omoshiro_app.databinding.ActivitySplashBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -11,25 +13,41 @@ import kotlinx.coroutines.launch
 
 class SplashActivity: AppCompatActivity() {
 
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initBinding()
+        setUi()
+    }
+
+    private fun initBinding(){
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
+    }
+
+    private fun setUi(){
+
+        binding.image.visibility = View.INVISIBLE
+        setColor(R.color.blue_archive_white, R.color.blue_archive_blue);
 
         CoroutineScope(Dispatchers.Main).launch {
-            delay(2000)
+            delay(1500)
+            binding.image.visibility = View.VISIBLE
+            setColor(R.color.black, R.color.white);
+            delay(3000)
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
     }
 
-    fun initBinding(){
-        val binding = ActivitySplashBinding.inflate(layoutInflater)
-        binding.lifecycleOwner = this
-        val view = binding.root
-        setContentView(view)
+    private fun setColor(backgroundColorId: Int, textColorId: Int) {
+        binding.let {
+            it.rootView.setBackgroundColor(this.resources.getColor(backgroundColorId))
+            it.splashTitleText.setTextColor(this.resources.getColor(textColorId))
+        }
     }
-
 }
